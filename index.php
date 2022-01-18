7a1a0284030a91fa017c87079880bb69cdb1f42e
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-18 11:51:37 -0300

    Adicionado HTML Inicial

--- index.php
+++ index.php
@@ -1,14 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Gerenciador de Tarefas</title>
 </head>
+
 <body>
     <div class="container">
-        
+        <div class="header">
+            <h1>Gerenciador de Tarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_name"></label>
+                <input type="text" name="task_name" placeholder="Nome da tarefa">
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+        <div class="separator"></div>
+        <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa2</li>
+                <li>Tarefa3</li>
+                <li>tarefa 4 </li>
+            </ul>
+        </div>
+        <div class="footer">
+
+            Desenvolvido por Gabriel Martins
+        </div>
     </div>
 </body>
+
 </html>
