0380a05e8afb95e6cd0baadd645ace2b06dfe4f6
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-31 12:15:02 -0300

    Cadastrando com INSERT uma tarefa no banco

--- task.php
+++ task.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ .'./connect.php';
+require __DIR__ . './connect.php';
 
 session_start();
 
@@ -22,11 +22,17 @@ if (isset($_POST['task_name'])) {
             'task_date' => $_POST['task_date'],
             'task_image' => $file_name
         ];
-        
 
-        // Exclui 3 unset que Havia esquecido se necessario Pegar no Commit Anterior
-        array_push($_SESSION['tasks'], $data);
-        header('Location:index.php');
+        // Como o nome diz, este método apenas prepara uma operação no banco de dados, logo se faz necessário a utilização de outros métodos como execute, para realmente executar a operação
+        $stmt = $conn->prepare('INSERT INTO task (task_name, task_description, task_image, task_date)
+                                VALUES (:name, :description, :image, :date)');
+        // Para realizar a definição do valor de um parâmetro, esse método recebe como argumento  uma referência, ou seja, uma variável ou uma constante. Isso significa que se informarmos um valor de maneira direta para ele, não irá funcionar
+        $stmt->bindParam('name', $_POST['task_name']);
+        $stmt->bindParam('description', $_POST['task_description']);
+        $stmt->bindParam('image', $file_name);
+        $stmt->bindParam('date', $_POST['task_date']);
+
+        
 
     } else {
         $_SESSION['message'] = 'O campo nome da tarefa não pode estar vazio!';
