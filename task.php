8903ba7d0d35bf1d2c5db2f88e6db83aea643a69
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-31 14:21:08 -0300

    Deletando tarefa com Logica de Instruçao SQL

--- task.php
+++ task.php
@@ -16,15 +16,8 @@ if (isset($_POST['task_name'])) {
             move_uploaded_file($_FILES['task_image']['tmp_name'], $dir . $file_name);
         }
 
-        $data = [
-            'task_name' => $_POST['task_name'],
-            'task_description' => $_POST['task_description'],
-            'task_date' => $_POST['task_date'],
-            'task_image' => $file_name
-        ];
-
         // Como o nome diz, este método apenas prepara uma operação no banco de dados, logo se faz necessário a utilização de outros métodos como execute, para realmente executar a operação
-        $stmt = $conn->prepare('INSERT INTO task (task_name, task_description, task_image, task_date)
+        $stmt = $conn->prepare('INSERT INTO tasks (task_name, task_description, task_image, task_date)
                                 VALUES (:name, :description, :image, :date)');
         // Para realizar a definição do valor de um parâmetro, esse método recebe como argumento  uma referência, ou seja, uma variável ou uma constante. Isso significa que se informarmos um valor de maneira direta para ele, não irá funcionar
         $stmt->bindParam('name', $_POST['task_name']);
@@ -32,7 +25,7 @@ if (isset($_POST['task_name'])) {
         $stmt->bindParam('image', $file_name);
         $stmt->bindParam('date', $_POST['task_date']);
 
-        // Excluido o Array push e unset
+        // Excluido o Array push e unset que estavam aqui
         if ($stmt->execute()) {
             $_SESSION['success'] = "Dados cadastrados!";
             header('Location:index.php');
@@ -40,7 +33,6 @@ if (isset($_POST['task_name'])) {
             $_SESSION['error'] = "Dados não cadastrados!";
             header('Location:index.php');
         }
-
     } else {
         $_SESSION['message'] = 'O campo nome da tarefa não pode estar vazio!';
         header('Location:index.php');
@@ -49,7 +41,14 @@ if (isset($_POST['task_name'])) {
 
 
 if (isset($_GET['key'])) {
-    array_splice($_SESSION['tasks'], $_GET['key'], 1);
-    unset($_GET['key']);
-    header('Location:index.php');
+    $stmt = $conn->prepare('DELETE FROM tasks WHERE id = :id');
+    $stmt->bindParam(':id', $_GET['key']);
+
+    if ($stmt->execute()) {
+        $_SESSION['success'] = "Dados Removidos !";
+        header('Location:index.php');
+    } else {
+        $_SESSION['error'] = "Dados não Removidos !";
+        header('Location:index.php');
+    }
 }
