cf8d04392e18f4ccd34e312ed7aa42619afd58cc
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-31 14:28:42 -0300

    Excluindo echo de Sucesso

--- connect.php
+++ connect.php
@@ -2,7 +2,6 @@
 
 try {
     $conn = new pdo('mysql:host=localhost;dbname=gerenciadorTarefas', 'root', '');
-    echo "Conectou";
 } catch (PDOException $e) {
     echo "Erro ao se conectar: Erro " . $e->getMessage();
 }
