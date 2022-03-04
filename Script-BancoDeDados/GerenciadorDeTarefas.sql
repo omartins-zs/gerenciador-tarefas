25057cf33b71a66a0c8918ab0ac61e7d287a0e52
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-04 11:27:03 -0300

    Adicionando Script de Criação do BD

---
+++ Script-BancoDeDados/GerenciadorDeTarefas.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE gerenciadorTarefas
+
+GO
+
+USE gerenciadorTarefas
+GO
+
+CREATE TABLE tasks (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    task_name VARCHAR(150) ,
+    task_description VARCHAR(150),
+    task_image VARCHAR(100),
+    task_date DATE
+)
+GO
+
+
+INSERT INTO `tasks` (`id`, `task_name`, `task_description`, `task_image`, `task_date`) VALUES (NULL, 'teste', 'teste de descriçao', NULL, '2022-01-01'), (NULL, 'teste 2', 'teste 2 descricao', NULL, '2022-01-31')
